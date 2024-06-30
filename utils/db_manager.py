778e9858fbf1de016fdbb8f21d7788e78740c195
alinaghania <alinaghani13@gmail.com>
2024-06-30 18:28:50 +0200

    prompt battle

--- utils/db_manager.py
+++ utils/db_manager.py
@@ -1,4 +1,5 @@
-from pymongo import MongoClient
+import pymongo
+
 from urllib.parse import quote_plus
 import streamlit as st
 
@@ -6,5 +7,7 @@ def init_connection():
     username = quote_plus(st.secrets["mongo"]["username"])
     password = quote_plus(st.secrets["mongo"]["password"])
     uri = f"mongodb+srv://{username}:{password}@cluster0.q3crdzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    client = MongoClient(uri)
+    client = pymongo.MongoClient(uri)
     return client
+
+client = init_connection()
