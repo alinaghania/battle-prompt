c163731735ee48ce942a9ac5e80959e4fd3cc122
alinaghania <alinaghani13@gmail.com>
2024-06-30 19:42:11 +0200

    Your commit message

--- utils/dalle.py
+++ utils/dalle.py
@@ -3,16 +3,29 @@ import streamlit as st
 import urllib.request
 
 # Access the API key from Streamlit secrets
-openai.api_key = st.secrets["openai"]["api_key"]
+client = st.secrets["openai"]["api_key"]
+
+# def generate_image_openai(prompt):
+#     response = openai.Image.create(
+#         model="dall-e-3",
+#         prompt=prompt,
+#         n=1,
+#         size="1024x1024"
+#     )
+#     image_url = response['data'][0]['url']
+#     return image_url
+
 
 def generate_image_openai(prompt):
-    response = openai.Image.create(
+    response = client.images.generate(
         model="dall-e-3",
         prompt=prompt,
+        size="1024x1024",
+        quality="standard",
         n=1,
-        size="1024x1024"
     )
-    image_url = response['data'][0]['url']
+
+    image_url = response.data[0].url
     return image_url
 
 def save_img(img_url, file_path):
